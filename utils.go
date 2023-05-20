cbce8d73e3c23dc60d081e64e119eaf67199e870
Thim Cederluind <thim@cederlund.de>
2023-05-20 13:15:39 +0200

    utils: use a Encoder instead of marshaling is faster

--- utils.go
+++ utils.go
@@ -10,14 +10,7 @@ func writeResponse(res http.ResponseWriter, httpCode int, response ApiResponse) 
 	res.Header().Set("Content-type", "application/json")
 	res.WriteHeader(httpCode)
 
-	jsonData, err := json.Marshal(response)
-	if err != nil {
+	if err := json.NewEncoder(res).Encode(&response); err != nil {
 		log.Println(err)
-	}
-
-	_, err = res.Write(jsonData)
-	if err != nil {
-		log.Println(err)
-		return
 	}
 }
